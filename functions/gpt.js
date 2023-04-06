ac05c6d9bd84bdbc8f0ed329fed77e6c486067a0
David Kim <david.kim@halopowered.com>
2023-04-06 17:52:15 -0400

    add polyfill

--- functions/gpt.js
+++ functions/gpt.js
@@ -1,25 +1,38 @@
+import 'unfetch/polyfill';
+
 exports.handler = async (event, context) => {
-  const body = event.body;
+  try {
+    const body = JSON.parse(event.body);
+    console.log(event.body);
 
-  return import('chatgpt')
-    .then((gpt) => {
-      const api = new gpt.ChatGPTAPI({
-        apiKey: process.env.OPENAI_API_KEY
+    return import('chatgpt')
+      .then((gpt) => {
+        const api = new gpt.ChatGPTAPI({
+          apiKey: process.env.OPENAI_API_KEY
+        });
+  
+        console.log(body);
+  
+        if (body.payload !== undefined) {
+          return api.sendMessage(body.payload)
+            .then((response) => {
+              return {
+                statusCode: 200,
+                body: response.text,
+              };
+            });
+        } else {
+          return {
+            statusCode: 400,
+            body: 'Payload missing',
+          };
+        }
       });
+  } catch(e) {
+    return {
+      statusCode: 400,
+      body: 'Malformed Payload ' + e,
+    };
+  }
 
-      if (body.payload !== undefined) {
-        return api.sendMessage(body.payload)
-          .then((response) => {
-            return {
-              statusCode: 200,
-              body: response.text,
-            };
-          });
-      } else {
-        return {
-          statusCode: 400,
-          body: 'Payload missing',
-        };
-      }
-    });
 };
