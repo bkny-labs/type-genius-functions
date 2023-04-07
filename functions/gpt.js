8c2e452a15d0ca6791d9b53718661a7fb1b21e01
Shawn Marincas <shawngmarincas@gmail.com>
2023-04-06 20:20:58 -0400

    fix: refactor function to validate and return before module import

--- functions/gpt.js
+++ functions/gpt.js
@@ -1,39 +1,53 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 global.fetch = fetch;
 
 exports.handler = async (event, context) => {
-  try {
-    const body = JSON.parse(event.body);
-    console.log(event.body);
+  let body;
 
-    return import('chatgpt')
-      .then((gpt) => {
-        const api = new gpt.ChatGPTAPI({
-          apiKey: process.env.OPENAI_API_KEY
-        });
-  
-        console.log(body);
-  
-        if (body.payload !== undefined) {
-          return api.sendMessage(body.payload)
-            .then((response) => {
-              return {
-                statusCode: 200,
-                body: response.text,
-              };
-            });
-        } else {
-          return {
-            statusCode: 400,
-            body: 'Payload missing',
-          };
-        }
-      });
-  } catch(e) {
+  if (event.httpMethod !== "POST") {
     return {
-      statusCode: 400,
-      body: 'Malformed Payload ' + e,
+      statusCode: 405,
+      body: "Method Not Allowed",
     };
   }
 
+  if (!event.body || event.headers["content-type"] !== "application/json") {
+    return {
+      statusCode: 400,
+      body: "Bad Request",
+    };
+  }
+
+  console.log('parsing event body', event.body);
+  try {
+    body = JSON.parse(event.body);
+  } catch (e) {
+    console.log("bady body json", event.body);
+    return {
+      statusCode: 400,
+      body: "Malformed Payload" + e,
+    };
+  }
+
+  console.log(body.payload);
+  if (body.payload === undefined) {
+    return {
+      statusCode: 400,
+      body: "Payload Missing",
+    };
+  }
+
+  console.log('importing chatgpt');
+  return import("chatgpt").then((gpt) => {
+    const api = new gpt.ChatGPTAPI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    return api.sendMessage(body.payload).then((response) => {
+      return {
+        statusCode: 200,
+        body: response.text,
+      };
+    });
+  });
 };
