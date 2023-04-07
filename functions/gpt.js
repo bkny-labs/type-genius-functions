4948de54ad8192ab504bbb9674d6ee2fb8a1d4dd
Shawn Marincas <shawngmarincas@gmail.com>
2023-04-06 22:15:44 -0400

    fix: libraries? we don't need no stinkin' libraries.

--- functions/gpt.js
+++ functions/gpt.js
@@ -1,16 +1,10 @@
 import fetch from "node-fetch";
-global.fetch = fetch;
+
+const OPENAI_API_URL = `https://api.openai.com/v1/chat/completions`;
+const TYPE_GENIUS_SYSTEM_PROMPT =
+  "You are TypeGenius, an AI that helps people fill in inputs and text areas on any website. You will complete the sentence sent by the User and include no other output to your response.";
 
 exports.handler = async (event, context) => {
-  let body;
-
-  if (event.httpMethod !== "POST") {
-    return {
-      statusCode: 405,
-      body: "Method Not Allowed",
-    };
-  }
-
   if (!event.body || event.headers["content-type"] !== "application/json") {
     return {
       statusCode: 400,
@@ -18,36 +12,71 @@ exports.handler = async (event, context) => {
     };
   }
 
-  console.log('parsing event body', event.body);
+  let body;
   try {
     body = JSON.parse(event.body);
   } catch (e) {
-    console.log("bady body json", event.body);
     return {
       statusCode: 400,
       body: "Malformed Payload" + e,
     };
   }
 
-  console.log(body.payload);
-  if (body.payload === undefined) {
+  if (!body.payload) {
     return {
       statusCode: 400,
       body: "Payload Missing",
     };
   }
 
-  console.log('importing chatgpt');
-  return import("chatgpt").then((gpt) => {
-    const api = new gpt.ChatGPTAPI({
-      apiKey: process.env.OPENAI_API_KEY,
+  let gptResponse;
+  try {
+    gptResponse = await fetch(OPENAI_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        messages: [
+          {
+            role: "system",
+            content: TYPE_GENIUS_SYSTEM_PROMPT,
+          },
+          {
+            role: "user",
+            content: body.payload,
+          },
+        ],
+      }),
     });
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: "OpenAI API Error: " + e,
+    };
+  }
 
-    return api.sendMessage(body.payload).then((response) => {
-      return {
-        statusCode: 200,
-        body: response.text,
-      };
-    });
-  });
+  if (gptResponse.status !== 200) {
+    const body = await gptResponse.text();
+    return {
+      statusCode: 500,
+      body: `Failed to send message. HTTP ${gptResponse.status} - ${body}`,
+    };
+  }
+
+  const gptPayload = await gptResponse.json();
+  const completion = gptPayload?.choices[0]?.message;
+
+  if (!reply) {
+    return {
+      statusCode: 500,
+      body: "Server Error: No reply from OpenAI",
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: completion,
+  };
 };
